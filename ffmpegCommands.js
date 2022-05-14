977077a2448f13027132a08757b1148ef17edb80
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-14 09:01:17 +0300

    Update ffmpegCommands.js

--- ffmpegCommands.js
+++ ffmpegCommands.js
@@ -2,4 +2,7 @@
 
 -i input.mp4 -vf fps=16,scale=600:-2:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=128:reserve_transparent=0[p];[s1][p]paletteuse -y output.gif
 
+//working on macos:
+ffmpeg  -i sbunny.mov -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 output.gif
+
 ffmpeg -i sbunny.mov -vf fps=16,scale=600:-2:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=128:reserve_transparent=0[p];[s1][p]paletteuse -y ptest7.gif
