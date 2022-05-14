43d8807d4fbab4dbcb023416c588eb74bbadf043
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-14 09:12:18 +0300

    Update ffmpegCommands.js

--- ffmpegCommands.js
+++ ffmpegCommands.js
@@ -4,5 +4,11 @@
 
 //working on macos:
 ffmpeg  -i sbunny.mov -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 output.gif
+ffmpeg  -i sbunny.mov -vf "fps=30,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 output.gif
+
+//new pallet each frame, (take a long time)
+ffmpeg  -i sbunny.mov -vf "fps=30,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen=stats_mode=single[p];[s1][p]paletteuse=new=1" -loop 0 output.gif
+
+
 
 ffmpeg -i sbunny.mov -vf fps=16,scale=600:-2:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=128:reserve_transparent=0[p];[s1][p]paletteuse -y ptest7.gif
